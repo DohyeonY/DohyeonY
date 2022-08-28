6c4d2b6afde98db83f441d3de0c535d85d7108f2
JUMURE <91860916+DohyeonY@users.noreply.github.com>
2022-08-28 15:24:39 +0900

    [Silver V] Title: 종이자르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -0,0 +1,29 @@
+W, H = map(int, input().split())
+
+N = int(input())
+arr = []
+for _ in range(N) :
+    lst = list(map(int, input().split()))
+    arr.append(lst)
+
+Row = [0, W]
+Col = [0, H]
+for i in range(N) :
+    if arr[i][0] == 0 :
+        Col.append(arr[i][1])
+    else :
+        Row.append(arr[i][1])
+
+Row.sort()
+Col.sort()
+# print(Row, Col)
+cutrow = []
+cutcol = []
+for i in range(len(Row)-1) :
+    cutrow.append(Row[i+1] - Row[i])
+for i in range(len(Col)-1) :
+    cutcol.append(Col[i+1] - Col[i])
+# 
+# print(cutrow, cutcol)
+
+print(max(cutrow) * max(cutcol))
