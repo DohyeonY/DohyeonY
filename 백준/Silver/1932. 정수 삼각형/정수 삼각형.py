f4b3f9e42d52ef1f5150b285c53db774df8b1445
JUMURE <91860916+DohyeonY@users.noreply.github.com>
2022-12-04 01:04:03 +0900

    [Silver I] Title: 정수 삼각형, Time: 208 ms, Memory: 34040 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,14 @@
+num=int(input())
+arr=[]
+for i in range(num) :
+  arr.append(list(map(int, input().split())))
+
+for i in range(1, num) :
+  for j in range(len(arr[i])) :
+    if j == 0 :
+      arr[i][j] = arr[i][j] + arr[i-1][j]
+    elif j==len(arr[i])-1 : 
+      arr[i][j] = arr[i][j] + arr[i-1][j-1]
+    else :
+      arr[i][j] = max(arr[i-1][j-1], arr[i-1][j]) + arr[i][j]
+print(max(arr[num-1]))
